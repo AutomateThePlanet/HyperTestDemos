599af9883878b4e032732a8110bc1b1aaea454b3
Anton Angelov <aangelov@automatetheplanet.com>
2022-02-14 13:25:14 +0200

    modify all examples

--- HyperTestDemo/DriverFactory.cs
+++ HyperTestDemo/DriverFactory.cs
@@ -8,35 +8,48 @@ using System.Threading;
 using WebDriverManager;
 using WebDriverManager.DriverConfigs.Impl;
 
-namespace HyperTestDemo;
-
-public class DriverFactory
+namespace HyperTestDemo
 {
-    static DriverFactory()
+    public class DriverFactory
     {
-        new DriverManager().SetUpDriver(new ChromeConfig());
-    }
+        static DriverFactory()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+        }
 
-    public static IWebDriver GetDriver(string testName, int makeSlowerFactor = 1)
-    {
-        string username = Environment.GetEnvironmentVariable("LT_USERNAME");
-        string authkey = Environment.GetEnvironmentVariable("LT_ACCESSKEY");
+        public static IWebDriver GetDriver(string testName, int makeSlowerFactor = 1)
+        {
+            string username = Environment.GetEnvironmentVariable("LT_USERNAME");
+            string authkey = Environment.GetEnvironmentVariable("LT_ACCESSKEY");
 
-        var capabilities = new ChromeOptions();
-        capabilities.BrowserVersion = "97.0";
-        var ltOptions = new Dictionary<string, object>();
-        ltOptions.Add("user", username);
-        ltOptions.Add("accessKey", authkey);
-        ltOptions.Add("build", "your build name");
-        ltOptions.Add("name", testName);
-        ltOptions.Add("platformName", "Windows 10");
-        ltOptions.Add("selenium_version", "4.0.0");
-        capabilities.AddAdditionalOption("LT:Options", ltOptions);
-        IWebDriver driver = new RemoteWebDriver(new Uri("https://" + username + ":" + authkey + "@hub.lambdatest.com/wd/hub"), capabilities);
-        EventFiringWebDriver firedDriver = new EventFiringWebDriver(driver);
-        firedDriver.FindingElement += (s, e) => Thread.Sleep(makeSlowerFactor * 500);
-        firedDriver.Manage().Window.Maximize();
+            ////var capabilities = new ChromeOptions();
+            ////capabilities.BrowserVersion = "97.0";
+            ////var ltOptions = new Dictionary<string, object>();
+            ////ltOptions.Add("user", username);
+            ////ltOptions.Add("accessKey", authkey);
+            ////ltOptions.Add("build", "your build name");
+            ////ltOptions.Add("name", testName);
+            ////ltOptions.Add("platformName", "Windows 10");
+            ////ltOptions.Add("selenium_version", "3.13.0");
+            ////ltOptions.Add("driver_version", "98.0");
+            ////capabilities.AddAdditionalCapability("LT:Options", ltOptions);
 
-        return firedDriver;
+            var capabilities = new DesiredCapabilities();
+            capabilities.SetCapability("username", username);
+            capabilities.SetCapability("accesskey", authkey);
+            capabilities.SetCapability("geoLocation", "US");
+
+            capabilities.SetCapability("build", "HyperTest Demo for Geekle US");
+            capabilities.SetCapability("name", "HyperTest Demo for Geekle US");
+            capabilities.SetCapability("platformName", "Windows 10");
+            capabilities.SetCapability("browser", "Chrome");
+
+            IWebDriver driver = new RemoteWebDriver(new Uri("https://" + username + ":" + authkey + "@hub.lambdatest.com/wd/hub"), capabilities);
+            EventFiringWebDriver firedDriver = new EventFiringWebDriver(driver);
+            firedDriver.FindingElement += (s, e) => Thread.Sleep(makeSlowerFactor * 50);
+            firedDriver.Manage().Window.Maximize();
+
+            return firedDriver;
+        }
     }
 }
--- HyperTestDemo/InitializeStatements.cs
+++ HyperTestDemo/InitializeStatements.cs
@@ -1,4 +1,4 @@
-﻿global using System;
-global using NUnit.Framework;
-global using OpenQA.Selenium;
-global using OpenQA.Selenium.Support.UI;
+﻿//global using System;
+//global using NUnit.Framework;
+//global using OpenQA.Selenium;
+//global using OpenQA.Selenium.Support.UI;
